206565381ca2944cf9c688450906f2c998b75be9
wangyao_tp <wangyao_tp@10.62.128.114>
2013-04-16 17:30:12 +0800

    1

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -14,6 +14,6 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        System.out.print("Hello world!!");
     }
 }
