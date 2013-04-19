68c97a77473035a72074e638de196d80c3f80169
wangyao_tp <wangyao_tp@10.62.128.114>
2013-04-19 16:07:54 +0800

    1

---
+++ src/helloworld/AliasTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+import static net.mindview.util.Print.*;
+class Tank{
+    public float level;
+}
+
+/**
+ *
+ * @author wangyao_tp
+ */
+public class AliasTest {
+    
+    public static void main(String[] args){
+        Tank t1 = new Tank();
+        Tank t2 = new Tank(); 
+        t1.level=2.5f;
+        t2.level=3;
+        print("t1:"+t1.level+" t2:"+t2.level);
+        t1 = t2;
+        print("t1:"+t1.level+" t2:"+t2.level);
+        t1.level = 13;
+        t2.level = 3.3f;
+        print("t1:"+t1.level+" t2:"+t2.level);
+    }   
+}
