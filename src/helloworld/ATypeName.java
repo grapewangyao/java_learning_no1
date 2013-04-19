5bb1588ac6cbaead31ef8c1af9b9b3f53546dd0b
wangyao_tp <wangyao_tp@10.62.128.114>
2013-04-19 08:22:43 +0800

    1

---
+++ src/helloworld/ATypeName.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author wangyao_tp
+ */
+public class ATypeName {
+        int a = 1;
+        double b = 3;
+        boolean c = false;
+        
+        public void show(){
+            System.out.print("a:" + a+"b:"+b+"c:"+c+"\n");
+        }
+}
