59a601575084b9feadf005166e2a9d97ac688642
wangyao_tp <wangyao_tp@10.62.128.114>
2013-04-19 08:24:18 +0800

    1

---
+++ src/helloworld/Test1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author wangyao_tp
+ */
+public class Test1 {
+    static int test1 = 123;
+    static char x;
+    
+    public static void main(String[] arg){
+        System.out.println(test1);
+        System.out.print(x);
+    }
+}
